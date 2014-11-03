735e7c5479591dca24010ac10c17d6fadc2ae015
Sampsa Laapotti <sampoz@iki.fi>
2014-11-04 00:22:25 +0200

    First alpha version

--- initscript.sh
+++ initscript.sh
@@ -1,1 +1,10 @@
+#! /bin/bash
 echo "make right dirs and make thumbnails"
+mkdir thumbnails
+#rename all to lowercase TODO
+cd images
+for file in *.JPG; do convert $file -resize 20% ../thumbnails/$file; done
+cd ..
+echo "window.thumbnails = [ " > thumbnails.js
+ls -1 thumbnails | awk '{printf("%s%s%s,\n","\"",$0,"\"")}' >> thumbnails.js
+echo "];" >> thumbnails.js
