8edbb1deaa384fcc6d855c88a40b87464dd71885
Sampsa Laapotti <sampoz@iki.fi>
2014-11-16 22:15:52 +0200

    better conversion parameters

--- initscript.sh
+++ initscript.sh
@@ -11,7 +11,7 @@ exiftran -ia *
 echo "create thumbnails from original images"
 for file in *.JPG;do
 	echo "converting file " $file
-	convert $file -resize 10% -quality 40 ../thumbnails/$file; 
+	convert $file -resize 7% -quality 85 ../thumbnails/$file; 
 done
 cd ..
 bash gifs.sh
